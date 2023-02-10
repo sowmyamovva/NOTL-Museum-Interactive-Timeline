7507668c587ec91cb80333945f2a9115dff69df3
Rimpy Saha <75136650+SahaRimpy@users.noreply.github.com>
2023-02-10 11:11:26 -0500

    Update db.php

--- Project_Source_Code/NOTL/includes/db.php
+++ Project_Source_Code/NOTL/includes/db.php
@@ -1,22 +1,12 @@
 <?php
-    // $server = "sql106.epizy.com";
-    // $username = "epiz_33561013";
-    // $password = "yr78nEbN65Ffp";
-    // $dbname = "epiz_33561013_timeline";
 
-    // $conn = mysqli_connect($server,$username,$password,$dbname);
-
-    // if(!$conn)
-    // {
-    //     die("Connection has Failed: ".mysqli_connect_error());
-    // }
 
     function OpenCon()
     {
-        $server = "sql106.epizy.com";
-        $username = "epiz_33561013";
-        $password = "yr78nEbN65Ffp";
-        $dbname = "epiz_33561013_timeline";
+        $server = "";
+        $username = "";
+        $password = "";
+        $dbname = "";
 
         $conn = new mysqli($server,$username,$password,$dbname) or die("Connect failed: %s\n". $conn -> error);
         
