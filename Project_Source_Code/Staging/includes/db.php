ecc10f9273f1b18a4b54ddd9e861ffcce93813fc
Rimpy Saha <75136650+SahaRimpy@users.noreply.github.com>
2023-02-10 11:05:01 -0500

    Create db.php

---
+++ Project_Source_Code/Staging/includes/db.php
@@ -0,0 +1,33 @@
+<?php
+    // $server = "sql106.epizy.com";
+    // $username = "epiz_33561013";
+    // $password = "yr78nEbN65Ffp";
+    // $dbname = "epiz_33561013_timeline";
+
+    // $conn = mysqli_connect($server,$username,$password,$dbname);
+
+    // if(!$conn)
+    // {
+    //     die("Connection has Failed: ".mysqli_connect_error());
+    // }
+
+    function OpenCon()
+    {
+        $server = "sql106.epizy.com";
+        $username = "epiz_33561013";
+        $password = "yr78nEbN65Ffp";
+        $dbname = "epiz_33561013_timeline";
+
+        $conn = new mysqli($server,$username,$password,$dbname) or die("Connect failed: %s\n". $conn -> error);
+        
+        return $conn;
+    }
+    
+    function CloseCon($conn)
+    {
+        $conn -> close();
+    }
+
+
+
+?>
