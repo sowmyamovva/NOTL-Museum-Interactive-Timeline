2e7e2395f88f15febb9b1626b4786b9b77bd7172
Tyvon <122844175+TyvonFG@users.noreply.github.com>
2023-03-15 23:11:04 -0400

    Update experimental.js
    Updated JS to conform to dynamic standards

--- Project_Source_Code/Staging/js/experimental.js
+++ Project_Source_Code/Staging/js/experimental.js
@@ -1,57 +1,64 @@
 var years = ["1810", "1811", "1812", "1813", "1814", "1815", "1816", "1817", "1818", "1819"];
-// var info = ["At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium"];
 
-// var info = [
-//   "<div class='overlay-container'>",
-//   "<img src='img/1700s.png' alt='1700s' class='overlay' title='This is an image of the 1700s'>",
-//   "<img src='img/1730s.webp' alt='1730s' class='overlay' title='This is an image of the 1730s'>",
-//   "<img src='img/1753.png' alt='1753s' class='overlay' title='This is an image of the 1753s'>",
-//   "<img src='img/1760.png' alt='1760s' class='overlay' title='This is an image of the 1760s'>",
-//   "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-//   "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-//   "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-//   "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-//   "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-//   "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-//   "</div>"
-// ];
+var user_images = [ // User's Images
+  'img/card_front.png',
+  'img/card_back.png',
+  
+  'img/card_front.png',
+  'img/card_back.png',
 
-var info = [
-  "<img src='img/1700s.png' alt='1700s' class='overlay' title='This is an image of the 1700s'>",
-  "<div class='flip-card ' onclick='flipImage(this)'><div class='flip-card-inner'><div class='flip-card-front'><img src='img/card_front.png' alt='1730s' class='overlay' title='This is an image of the 1730s'></div><div class='flip-card-back'><img src='img/card_back.png' alt='1776s' class='overlay' title='This is an image of the 1776s' width='400' height='300'></div></div></div>",
-  "<div class='flip-card'><div class='flip-card-inner'><div class='flip-card-front'><img src='img/card_front.png' alt='1730s' class='overlay' title='This is an image of the 1730s'></div><div class='flip-card-back'><img src='img/card_back.png' alt='1776s' class='overlay' title='This is an image of the 1776s'></div></div></div>",
-  "<div class='flip-container'><div class='flipper'><div class='front'><img src='img/card_front.png' alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src='img/card_back.png' alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
-  "<div class = 'image-container'><img src='img/1753.png' alt='1753s' class='overlay back-image' title='This is an image of the 1753s' onclick='swapImages()'><img src='img/1700s.png' alt='1700s' class='overlay front-image' title='This is an image of the 1700s' onclick='swapImages()'></div>", // Stack tester
-  "<img src='img/1760.png' alt='1760s' class='overlay' title='This is an image of the 1760s'>",
-  "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-  "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-  "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-  "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-  "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-  "<img src='img/1776.png' alt='1776s' class='overlay' title='This is an image of the 1776s'>",
-  "<div class='overlay'>",
-  "<div class='overlay-content'>",
-  "<div class='flip-card'>",
-  "<div class='flip-card-inner'>",
-  "<div class='flip-card-front'>",
-  "<img src='' alt=''>",
-  "</div>",
-  "<div class='flip-card-back'>",
-  "<img src='' alt=''>",
-  "</div>",
-  "</div>",
-  "</div>",
-  "</div>",
-  "</div>",
-  "</div>",
-  "</div>"
+  'img/card_front.png',
+  'img/card_back.png',
+
+  'img/card_front.png',
+  'img/card_back.png',
+
+  'img/card_front.png',
+  'img/card_back.png',
+
+  'img/card_front.png',
+  'img/card_back.png',
+
+  'img/card_front.png',
+  'img/card_back.png',
+
+  'img/card_front.png',
+  'img/card_back.png',
+
+  'img/card_front.png',
+  'img/card_back.png',
 ];
+
+var info = [ // Processed info
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[0]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[1]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[2]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[3]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[4]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[5]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[6]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[7]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[8]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[9]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[10]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[11]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[12]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[13]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[14]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[15]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[16]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[17]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+  "<div class='flip-container'><div class='flipper'><div class='front'><img src="+user_images[16]+" alt='1730s' class='overlay' title='This is an image of the 1730s'> </div><div class='back'><img src="+user_images[17]+" alt='1753s' class='overlay' title='This is an image of the 1753s'></div></div></div>",
+]
+
+
+// Swapper Function 
+//   "<div class='flip-container'><div class='flipper'><div class='front'><img src='img/card_front.png' alt='1730s' class='overlay front-image' title='This is an image of the 1730s' onclick='swapImages()> </div><div class='back'><img src='img/card_back.png' alt='1753s' class='overlay back-image' title='This is an image of the 1753s'></div></div></div><img src='img/1753.png' alt='1753s' class='overlay front-image' title='This is an image of the 1753s' onclick='swapImages()'>",
+// Click Function
+// "<div class='flip-card ' onclick='flipImage(this)'><div class='flip-card-inner'><div class='flip-card-front'><img src='img/card_front.png' alt='1730s' class='overlay' title='This is an image of the 1730s'></div><div class='flip-card-back'><img src='img/card_back.png' alt='1776s' class='overlay' title='This is an image of the 1776s' width='400' height='300'></div></div></div>",
+
+var content = [
+"img/1776.png"+'',
+"img/1777.png",
+"img/1778.png"
+]
+
 
 //
 for (let i = 0; i < years.length; i++) {
   pathOnDiv(years[i], (i / (years.length - 1)));
 }
-
 
 
 function pathOnDiv(text, pos) {
@@ -105,30 +112,6 @@ events.forEach(event => {
 function flipImage(card) {
   card.classList.toggle("flip-card-flipped");
 }
-
-// Alert
-// events.forEach(event => {
-//   // add click event listener to circle element
-//   event.addEventListener('click', () => {
-//     // display alert with message when circle is clicked
-//     alert('Circle with data-year="' + event.getAttribute('data-year') + '" was clicked!');
-//   });
-// });
-
-
-// Message
-// const messageElement = document.getElementsByClassName('message');
-
-// events.forEach(event => {
-//   // add click event listener to circle element
-//   event.addEventListener('click', () => {
-//     // set message with data-year when circle is clicked
-//     if (messageElement) {
-//       messageElement.innerHTML = 'Circle with data-year="' + event.getAttribute('data-year') + '" was clicked!';
-//     }
-//   });
-// });
-
 
 // Stack images Swapping function
 function swapImages() {
