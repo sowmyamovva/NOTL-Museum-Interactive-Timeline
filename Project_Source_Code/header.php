e0a9846c63da38bd69d147d4ff53d61f002bbc16
sowmyamovva <sm17zq@brocku.ca>
2023-03-28 20:47:45 -0400

    Footer php file

---
+++ Project_Source_Code/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
+    <!-- styles -->
+    <link rel="stylesheet" href="style.css" /> 
+    <title>Homepage</title> </head>
+
+<nav>
+    <div class="nav-center">
+        <!-- nav header -->
+        <div class="nav-header">
+            <h2>NOTL</h2>
+            <!-- <img src="./logo.svg" class="logo" alt="logo" /> -->
+            <button class="nav-toggle"> <i class="fas fa-bars"></i> </button>
+        </div>
+        <!-- links -->
+        <ul class="links">
+            <li> <a href="index.html">home</a> </li>
+            <li> <a href="about.html">Services</a> </li>
+            <li> <a href="projects.html">Visit Us</a> </li>
+            <li> <a href="contact.html">Contact</a> </li>
+        </ul>
+        <!----Searchbar goes here ----->
+        <ul class="container">
+            <form action="" class="SearchBar">
+                <input type="text" placeholder="Search" name="notlsearch">
+                <button type="submit"><img src="/Users/sowmyamovva/Desktop/SearchMag.png"></button>
+            </form>
+        </ul>
+    </div>
+</nav>
