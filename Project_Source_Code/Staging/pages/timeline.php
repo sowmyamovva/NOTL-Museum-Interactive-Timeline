feca5dad56e56ef8c9c8744e907f30d6f9008620
Tyvon <122844175+TyvonFG@users.noreply.github.com>
2023-04-14 00:38:44 -0400

    Update timeline.php
    Changed viewBox, and made timeline bigger.
    Added Calendar Feature

--- Project_Source_Code/Staging/pages/timeline.php
+++ Project_Source_Code/Staging/pages/timeline.php
@@ -84,11 +84,12 @@ CloseCon($conn);
     </div>
   </div>
 
+<button onclick="toggleCalendar()">Toggle Calendar</button>
+<div id="calendar"></div>
+    
 <div id="timeline_container" class="scroll-container">
-
   <div id="timeline_box" class="scroll-content">
-
-    <svg id="timeline" cache-id="16de89faabdb48d1a0a46f23dde4f4b1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 400 400" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"  x="0px" y="0px">
+    <svg id="timeline" cache-id="16de89faabdb48d1a0a46f23dde4f4b1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 300 8500 1" shape-rendering="geometricPrecision" text-rendering="geometricPrecision"  x="0px" y="0px">
       <defs>
         <linearGradient id="e0YvEuspUTQ2-stroke" x1="0" y1="150" x2="300" y2="150" spreadMethod="pad" gradientUnits="userSpaceOnUse" gradientTransform="translate(0 0)">
           <!-- <stop id="e0YvEuspUTQ2-stroke-0" offset="0%" stop-color="#fc259b" />
@@ -98,7 +99,7 @@ CloseCon($conn);
         </linearGradient>
       </defs>
 
-     <line id="mypath" x1="-1600" y1="180" x2="2000" y2="180" fill="#1c278a" stroke="url(#e0YvEuspUTQ2-stroke)" stroke-width="13" />
+     <line id="mypath" x1="300" y1="180" x2="7900" y2="180" fill="#1c278a" stroke="url(#e0YvEuspUTQ2-stroke)" stroke-width="13" />
     
         <g id ="sub_timeline" class="arrow_sub">
             <style type="text/css">
