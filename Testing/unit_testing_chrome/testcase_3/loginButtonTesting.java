4088435cd5ac65b35a63154915fff660fc4b5b5a
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-25 22:24:48 -0400

    unit level test case #3 for the chrome browser

---
+++ Testing/unit_testing_chrome/testcase_3/loginButtonTesting.java
@@ -0,0 +1,41 @@
+package testcase_3;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class loginButtonTesting {
+
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://badger-timeline.infinityfreeapp.com/public_html/");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/nav/div/ul[1]/form/a")));
+
+        element.click();
+
+    }
+
+    public static void main (String[] args){
+
+    }
+}
