2bf28bcd90c5e7d649744aaa87be2f3f79abe2b5
Tyvon <122844175+TyvonFG@users.noreply.github.com>
2023-04-26 16:29:54 -0400

    Updated Calendar features

--- Project_Source_Code/Staging/js/calendar.js
+++ Project_Source_Code/Staging/js/calendar.js
@@ -3,17 +3,21 @@ var dates = ['2023-04-01', '2023-04-05', '2023-04-10', '2023-04-15'];
 
 // eventName, calendarName, eventColor (possible colors include only blue, yellow, green, and orange)
 
-var myEvent = [ "Exhibit 1: 1812, Exhibit 1, blue",
-                "Exhibit 2: Slavery, Exhibit 2, yellow",
-                "Exhibit 3: Niagara Falls, Exhibit 3, green",
-                "Exhibit 4: Native, Exhibit 4, orange"]
+var myEvent = [ "Event 1: 1812, Event 1, blue",
+                "Event 2: Slavery, Event 2, yellow",
+                "Event 3: Niagara Falls, Event 3, green",
+                "Event 4: Native, Event 4, orange"]
 
-var 
+const cDict = { };
+const bookings = { };
+var bookingOverlay;
 
 !function() {
-
+    // The current date
     var today = moment();
-  
+
+    var selected_day;
+
     function Calendar(selector, events) {
       this.el = document.querySelector(selector);
       this.events = events;
@@ -33,7 +37,7 @@ var
       toggleButton.addEventListener('click', function() {
         self.el.classList.toggle('hide-calendar');
       });
-      this.header.appendChild(toggleButton);
+      // this.header.appendChild(toggleButton);
 
       // Hide calendar initially
       this.el.classList.add('hide-calendar');
@@ -74,11 +78,44 @@ var
           left.addEventListener('click', function() { self.prevMonth(); });
       
           //Append the Elements
-          // this.header.appendChild(this.toggleButton);
           this.header.appendChild(this.title); 
           this.header.appendChild(right);
           this.header.appendChild(left);
+          var button = createElement('button','cButton','View Bookings');
+
+          button.style.top = '2px';
+          button.style.position = 'absolute';
+          button.style.left = '50px';
+          button.style.height = '40px';
+          button.style.width = '80px';
+          this.header.append(button);
           this.el.appendChild(this.header);
+
+          // Create container for the overlay
+          var overlayContainer = createElement('div', 'overlay-container');
+          this.el.appendChild(overlayContainer);
+
+
+          var overlay = document.createElement('div');
+          overlay.className = 'bookings_overlay';
+          overlayContainer.appendChild(overlay);
+          overlay.style.display = 'none';
+          bookingOverlay = overlay; // set this so we can get it in other functions
+          // Add event listener to button
+          button.addEventListener('click', function() {
+            // Toggle 'active' class on button element
+            button.classList.toggle('active');
+
+            // Toggle bookings overlay on and off
+            var overlay = document.querySelector('.bookings_overlay');
+            if (overlay.style.display === 'none') {
+              overlay.style.display = 'block';
+            } else {
+              overlay.style.display = 'none';
+            }
+
+
+          });
         }
       
         this.title.innerHTML = this.current.format('MMMM YYYY');
@@ -92,7 +129,6 @@ var
     Calendar.prototype.drawMonth = function() {
       var self = this;
 
-    // Experimental
 
 
     // Loop through each event and assign a date from the dates array
@@ -227,7 +263,7 @@ var
       var details, arrow;
       var dayNumber = +el.querySelectorAll('.day-number')[0].innerText || +el.querySelectorAll('.day-number')[0].textContent;
       var day = this.current.clone().date(dayNumber);
-  
+      selected_day = day._d;
       var currentOpened = document.querySelector('.details');
   
       //Check to see if there is an open detais box on the current row
@@ -276,7 +312,8 @@ var
   
       arrow.style.left = el.offsetLeft - el.parentNode.offsetLeft + 27 + 'px';
     }
-  
+    
+    // This funcion is for when a day is clicked (cday)
     Calendar.prototype.renderEvents = function(events, ele) {
       //Remove any events in the current details element
       var currentWrapper = ele.querySelector('.events');
@@ -291,19 +328,92 @@ var
         div.appendChild(span);
         wrapper.appendChild(div);
       });
-  
+      
+      // If no events are present, add time slots
       if(!events.length) {
+        var temp = selected_day.toDateString(); // format of date to get day "Wed Apr 19 2023"
+        var temp2 = temp.split(' ');
+        var day = temp2[2];
+        var month = selected_day.getMonth();
+
+        const key = month.toString()+day.toString();
+        if (!(key in cDict)){ // If key doesn't exist in dict, create new one
+          cDict[key] = [0,0,0,0,temp];
+          bookings[key] = [''];
+          bookingOverlay.style.overflow = 'auto';
+        }
+
         var div = createElement('div', 'event empty');
         var span = createElement('span', '');
-        span.innerHTML = "Times&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Attendees<br>1:00 PM - 2:00 PM<br>2:00 PM - 3:00 PM<br>3:00 PM - 4:00 PM<br>4:00 PM - 5:00 PM"
+        span.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Times&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Attendees<br>1:00 PM - 2:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+cDict[key][0]+"/15<br>2:00 PM - 3:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+cDict[key][1]+"/15<br>3:00 PM - 4:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+cDict[key][2]+"/15<br>4:00 PM - 5:00 PM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+cDict[key][3]+"/15"
         // Add booking button and bookings
-        var booking_button = createElement('button','cButton','Book Viewing');
+        var button_1 = createElement('button','cButton','Book for 1PM');
+        button_1.id = 'button_1';
+        var button_2 = createElement('button','cButton','Book for 2PM');
+        button_2.id = 'button_2';
+        var button_3 = createElement('button','cButton','Book for 3PM');
+        button_3.id = 'button_3';
+        var button_4 = createElement('button','cButton','Book for 4PM');
+        button_4.id = 'button_4';
+        var button_5 = createElement('button','cButton','Book for 5PM');
+        button_5.id = 'button_5';
         
         
         div.appendChild(span);
         wrapper.appendChild(div);
-        div.append(booking_button);
-      }
+        div.append(button_1);
+        div.append(button_2);
+        div.append(button_3);
+        div.append(button_4);
+
+        // const bookings = { };
+        // var bookingOverlay;
+
+// Add a click event listener to the parent div
+div.addEventListener('click', function(event) {
+  // Check if the clicked element is a button
+  if (event.target.tagName === 'BUTTON') {
+    var textElement = createElement('div', 'event-text');
+
+    // Increment the count based on which button was clicked
+    switch (event.target.id) {
+      case 'button_1':
+        if (within_limit(cDict[key][0])){
+          cDict[key][0]+=1;
+          textElement.innerHTML = 'You have booked for the date ' + cDict[key][4] + ' for 1:00-2:00 PM.';
+          bookingOverlay.appendChild(textElement);
+        }
+        break;
+      case 'button_2':
+        if (within_limit(cDict[key][1])){
+          cDict[key][1]+=1;
+          textElement.innerHTML = 'You have booked for the date ' + cDict[key][4] + ' for 2:00-3:00 PM.';
+          bookingOverlay.appendChild(textElement);
+        }
+        break;
+      case 'button_3':
+        if (within_limit(cDict[key][2])){
+          cDict[key][2]+=1;
+          textElement.innerHTML = 'You have booked for the date ' + cDict[key][4] + ' for 3:00-4:00 PM.';
+          bookingOverlay.appendChild(textElement);
+        }
+        break;
+      case 'button_4':
+        if (within_limit(cDict[key][3])){
+          cDict[key][3]+=1;
+          textElement.innerHTML = 'You have booked for the date ' + cDict[key][4] + ' for 4:00-5:00 PM.';
+          bookingOverlay.appendChild(textElement);
+        }
+        break;
+      default:
+        break;
+    }
+    span.innerHTML = "Times&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Attendees<br>1:00 PM - 2:00 PM&nbsp;&nbsp;&nbsp;"+cDict[key][0]+"/15<br>2:00 PM - 3:00 PM&nbsp;&nbsp;&nbsp;"+cDict[key][1]+"/15<br>3:00 PM - 4:00 PM&nbsp;&nbsp;&nbsp;"+cDict[key][2]+"/15<br>4:00 PM - 5:00 PM&nbsp;&nbsp;&nbsp;"+cDict[key][3]+"/15"
+    console.log("");
+  }
+});
+console.log("");
+}
   
       if(currentWrapper) {
         currentWrapper.className = 'events out';
@@ -328,6 +438,9 @@ var
       }
     }
   
+  function within_limit(num){
+    return num < 15;
+  }  
     Calendar.prototype.drawLegend = function() {
       var legend = createElement('div', 'legend');
       var calendars = this.events.map(function(e) {
@@ -391,11 +504,11 @@ var
       
     }
 
-
-  
     var calendar = new Calendar('#calendar', data);
   }();
 
+  var cal = document.getElementById("calendar");
+  cal.style.display = "none";
   function toggleCalendar() {
     var calendar = document.getElementById("calendar");
     if (calendar.style.display === "none") {
@@ -407,45 +520,3 @@ var
   }
   
 
-
-
-
-
-
-
-
-
-//   !function() {
-//     var data = [
-//       { eventName: 'Lunch Meeting w/ Mark', calendar: 'Work', color: 'orange' },
-//       { eventName: 'Interview - Jr. Web Developer', calendar: 'Work', color: 'orange' },
-//       { eventName: 'Demo New App to the Board', calendar: 'Work', color: 'orange' },
-//       { eventName: 'Dinner w/ Marketing', calendar: 'Work', color: 'orange' },
-  
-//       { eventName: 'Game vs Portalnd', calendar: 'Sports', color: 'blue' },
-//       { eventName: 'Game vs Houston', calendar: 'Sports', color: 'blue' },
-//       { eventName: 'Game vs Denver', calendar: 'Sports', color: 'blue' },
-//       { eventName: 'Game vs San Degio', calendar: 'Sports', color: 'blue' },
-  
-//       { eventName: 'School Play', calendar: 'Kids', color: 'yellow' },
-//       { eventName: 'Parent/Teacher Conference', calendar: 'Kids', color: 'yellow' },
-//       { eventName: 'Pick up from Soccer Practice', calendar: 'Kids', color: 'yellow' },
-//       { eventName: 'Ice Cream Night', calendar: 'Kids', color: 'yellow' },
-  
-//       { eventName: 'Free Tamale Night', calendar: 'Other', color: 'green' },
-//       { eventName: 'Bowling Team', calendar: 'Other', color: 'green' },
-//       { eventName: 'Teach Kids to Code', calendar: 'Other', color: 'green' },
-//       { eventName: 'Startup Weekend', calendar: 'Other', color: 'green' }
-//     ];
-  
-//     function addDate(ev) {
-      
-//     }
-  
-//     var calendar = new Calendar('#calendar', data);
-  
-//   }();
-// eventName, calendarName, eventColor, date, endDate
-// var myEvent = "Exhibit, Work,blue,2023-03-28, 2023-03-29"
-
-// src: https://www.sliderrevolution.com/resources/html-calendar/
--- Project_Source_Code/Staging/main.js
+++ Project_Source_Code/Staging/main.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname)); // + '/Staging'
 
 
 app.get('',(req, res)=> {
-    res.sendFile(__dirname + '/pages/current_timeline.html');// timeline   current_timeline.html
+    res.sendFile(__dirname + '/pages/timeline.html');// timeline   current_timeline.html
 })
 // horizontal_example   // experimental
 
