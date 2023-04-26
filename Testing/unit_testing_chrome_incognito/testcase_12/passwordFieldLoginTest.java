65eee87523f565704154340b34e29150c27f1f86
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-26 02:44:19 -0400

    unit level test case #12 for the chrome browser in incognito mode

---
+++ Testing/unit_testing_chrome_incognito/testcase_12/passwordFieldLoginTest.java
@@ -0,0 +1,42 @@
+package testcase_12;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class passwordFieldLoginTest {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("incognito");
+        driver = new ChromeDriver(co);
+        driver.get("https://badger-timeline.infinityfreeapp.com/public_html/views/user/account");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[1]/form/div[2]/input"));
+        element.sendKeys("lebronDagoat");
+    }
+
+    public static void main (String[] args){
+
+    }
+}
