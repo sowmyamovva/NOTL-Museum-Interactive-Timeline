565ce138c0ef1691798bcca669fb9c2eb1057394
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-26 02:43:08 -0400

    unit level test case #9 for the chrome browser in incognito mode

---
+++ Testing/unit_testing_chrome_incognito/testcase_9/servicesLinkTest.java
@@ -0,0 +1,45 @@
+package testcase_9;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class servicesLinkTest {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("incognito");
+        driver = new ChromeDriver(co);
+        driver.get("https://badger-timeline.infinityfreeapp.com/src/timeline");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/nav/div/ul[1]/li[2]/a")));
+
+        element.click();
+
+    }
+
+    public static void main (String[] args){
+
+    }
+}
