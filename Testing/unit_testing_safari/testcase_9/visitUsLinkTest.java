3ca6c814afb78d4fef023b313b222c23b3db526b
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-26 18:57:46 -0400

    Unit level test case #9 for the Safari browser

---
+++ Testing/unit_testing_safari/testcase_9/visitUsLinkTest.java
@@ -0,0 +1,43 @@
+package testcase_9;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class visitUsLinkTest {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        driver = new SafariDriver();
+        driver.navigate().to("https://badger-timeline.infinityfreeapp.com/public_html/views/pages/timeline");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/nav/div/ul[1]/li[3]/a")));
+
+        element.click();
+
+    }
+
+    public static void main (String[] args){
+
+    }
+}
