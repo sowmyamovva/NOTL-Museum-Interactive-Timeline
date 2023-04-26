a14a15b3075f3e199b2a01618bf8bd56699cf250
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-26 18:57:22 -0400

    Unit level test case #8 for the Safari browser

---
+++ Testing/unit_testing_safari/testcase_8/fromInputTest.java
@@ -0,0 +1,40 @@
+package testcase_8;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class fromInputTest {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        driver = new SafariDriver();
+        driver.navigate().to("https://badger-timeline.infinityfreeapp.com/public_html/views/pages/timeline");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/input[1]"));
+        element.sendKeys("1700");
+    }
+
+    public static void main (String[] args){
+
+    }
+}
---
+++ Testing/unit_testing_safari/testcase_8/indigeniousHistorySelectionTest.java
@@ -0,0 +1,42 @@
+package testcase_8;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class indigeniousHistorySelectionTest {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        driver = new SafariDriver();
+        driver.navigate().to("https://badger-timeline.infinityfreeapp.com/public_html/views/pages/timeline");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/select"));
+        Select objectSelect = new Select(element);
+        objectSelect.selectByIndex(1);
+    }
+
+    public static void main (String[] args){
+
+    }
+}
