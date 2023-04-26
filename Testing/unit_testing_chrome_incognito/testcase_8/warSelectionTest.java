e7f0220c7b7ee8df89915dad068a7320467a2364
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-26 02:42:37 -0400

    unit level test case #8 for the chrome browser in incognito mode

---
+++ Testing/unit_testing_chrome_incognito/testcase_8/warSelectionTest.java
@@ -0,0 +1,44 @@
+package testcase_8;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class warSelectionTest {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        ChromeOptions co = new ChromeOptions();
+        co.addArguments("incognito");
+        driver = new ChromeDriver(co);
+        driver.get("https://badger-timeline.infinityfreeapp.com/src/timeline");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/select"));
+        Select objectSelect = new Select(element);
+        objectSelect.selectByIndex(3);
+    }
+
+    public static void main (String[] args){
+
+    }
+}
