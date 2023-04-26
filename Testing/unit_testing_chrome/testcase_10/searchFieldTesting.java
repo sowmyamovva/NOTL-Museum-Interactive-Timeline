d850d9e0810048acc3884ab745604164dc0bf41c
Peter Fung <80992941+datboypeter@users.noreply.github.com>
2023-04-25 22:28:53 -0400

    unit level test case #10 for the chrome browser

---
+++ Testing/unit_testing_chrome/testcase_10/searchFieldTesting.java
@@ -0,0 +1,39 @@
+package testcase_10;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class searchFieldTesting {
+    WebDriver driver = null;
+
+    @BeforeEach
+    public void beforeAll(){
+        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://badger-timeline.infinityfreeapp.com/src/timeline");
+    }
+
+    @AfterEach
+    public void aftertest(){
+        driver.quit();
+    }
+
+    @Test
+    public void test (){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement element = driver.findElement(By.xpath("/html/body/nav/div/ul[2]/div/form/input"));
+        element.sendKeys("put me in coach");
+    }
+
+    public static void main (String[] args){
+
+    }
+}
